10e20ee1b0c68da2d3589e0be6154ea79c29543d
gabriel <gabrielmartins1820@gmail.com>
2025-01-21 09:49:08 -0300

    Domain model

---
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -0,0 +1,63 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class BelongingPK {
+	
+	@ManyToOne
+	@JoinColumn(name = "game_id")
+	Game game;
+	
+	@ManyToOne
+	@JoinColumn(name = "list_id")
+	GameList gameList;
+	
+	public BelongingPK() {
+		
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(game, gameList);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPK other = (BelongingPK) obj;
+		return Objects.equals(game, other.game) && Objects.equals(gameList, other.gameList);
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	public GameList getGameList() {
+		return gameList;
+	}
+
+	public void setGameList(GameList gameList) {
+		this.gameList = gameList;
+	}
+
+	public BelongingPK(Game game, GameList gameList) {
+		super();
+		this.game = game;
+		this.gameList = gameList;
+	}
+	
+}
